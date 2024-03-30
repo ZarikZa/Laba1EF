67fa17d9d00029932d7ef1b0136ed9e3050abb24
ZarikZa <schemetov.ya@yandex.ru>
2024-03-30 09:29:02 +0300

    postavte 5

---
+++ Clientts.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabaBA1
+{
+    /// <summary>
+    /// Логика взаимодействия для Clientts.xaml
+    /// </summary>
+    public partial class Clientts : Page
+    {
+        private Laba1Entities1 Laba1Entities1 = new Laba1Entities1();
+        public Clientts()
+        {
+            InitializeComponent();
+            datygridy.IsReadOnly = true;
+            datygridy.ItemsSource = Laba1Entities1.Clients.ToList();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabaBA1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Laba1Entities1 Laba1Entities = new Laba1Entities1();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = @"Data Source=DESKTOP-H0SNFQR\SQLEXPRESS;Initial Catalog=Laba1;" +
+            "Integrated Security=SSPI;Pooling=False";
+
+            sqlConnection.Open();
+            DataTable schema = sqlConnection.GetSchema("Tables");
+            List<string> TableNames = new List<string>();
+            foreach (DataRow row in schema.Rows)
+            {
+                if (row[3].ToString() == "BASE TABLE" && row[2].ToString() != "sysdiagrams")
+                {
+                    TableNames.Add(row[2].ToString());
+                }
+            }
+            sqlConnection.Close();
+            TableNameComBox.ItemsSource = TableNames;
+            TableNameComBox.SelectedIndex = 0;
+        }
+
+        private void TableNameComBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string NameTable = TableNameComBox.SelectedItem.ToString();
+            if (NameTable == "Unitazs")
+            {
+                OtobrazenieFrame.Content = new Unitaz();
+            }
+            else if (NameTable == "Clients")
+            {
+                OtobrazenieFrame.Content = new Clientts();
+            }
+            else if(NameTable == "Orders")
+            {
+                OtobrazenieFrame.Content = new Orrders();
+            }
+            else if( NameTable == "UnitazTypes") 
+            {
+                OtobrazenieFrame.Content= new UnitazasTypes();
+            }
+        }
+    }
+}
